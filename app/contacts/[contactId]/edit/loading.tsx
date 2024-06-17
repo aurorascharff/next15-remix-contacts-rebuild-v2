83baebc55878cc76f446ba67a1b98fc834d53df1
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-06-17 17:52:11 +0200

    Update skeleton

--- app/contacts/[contactId]/edit/loading.tsx
+++ app/contacts/[contactId]/edit/loading.tsx
@@ -4,13 +4,13 @@ import Skeleton from '@/components/ui/Skeleton';
 export default function Loading() {
   return (
     <div className="grip-rows-6 grid max-w-[40rem] grid-cols-1 gap-4 sm:grid-cols-[1fr_4fr]">
-      <div className="flex flex-col gap-[72px] sm:gap-8">
+      <div className="hidden flex-col gap-[72px] sm:flex sm:gap-8">
         <span className="flex">Name</span>
         <div>Twitter</div>
         <div>Avatar URL</div>
         <div>Notes</div>
       </div>
-      <Skeleton className="hidden sm:block" />
+      <Skeleton />
     </div>
   );
 }
