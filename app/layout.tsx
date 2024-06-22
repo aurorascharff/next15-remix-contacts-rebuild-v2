97adf2f1341713cea3f758dbf7ab5e9c4eec89ca
Aurora Scharff <aurora.sofie@gmail.com>
2024-06-22 12:27:21 +0200

    Improve responsive sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en">
       <body className={inter.className}>
         <div className="flex w-full flex-col sm:flex-row">
-          <div className="flex h-1/3 w-full flex-col border-r border-gray bg-gray-light sm:h-auto sm:w-[16rem] md:w-[22rem]">
+          <div className="flex h-1/3 w-full flex-col border-r border-gray bg-gray-light sm:h-auto sm:w-[18rem] md:w-[22rem]">
             <div className="flex items-center gap-2 border-b border-gray px-8 py-4">
               <Search />
               <form action={createEmptyContact}>
