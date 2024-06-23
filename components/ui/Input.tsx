b6ecef6c3e7ddec823ea09e5534be8ec1da427bb
Aurora Scharff <aurora.sofie@gmail.com>
2024-06-23 22:45:55 +0200

    Update input css

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Input({ error, ...otherProps }: Props & React.InputHTMLAttributes<HTMLInputElement>) {
   return (
-    <div>
+    <div className="w-full">
       <input className={error ? 'outline outline-2 outline-destroy' : ''} aria-describedby="error" {...otherProps} />
       {error && <span className="text-destroy">{error}</span>}
     </div>
