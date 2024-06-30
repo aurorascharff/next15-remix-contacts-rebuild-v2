8a7dc0f3567edfd1e17157cdb5530cbeac9f0f75
Aurora Scharff <aurora.sofie@gmail.com>
2024-06-30 16:08:03 +0200

    UPDATE

--- lib/actions/updateContact.ts
+++ lib/actions/updateContact.ts
@@ -3,27 +3,22 @@
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { prisma } from '@/db';
-import type { ContactSchemaErrorType } from '@/validations/contactSchema';
+import type { ContactSchemaErrorType, ContactSchemaType } from '@/validations/contactSchema';
 import { contactSchema } from '@/validations/contactSchema';
 
 type State = {
   success?: boolean;
-  data?: Record<string, string>;
+  data?: ContactSchemaType;
   errors?: ContactSchemaErrorType;
 };
 
 export async function updateContact(contactId: string, _prevState: State, formData: FormData): Promise<State> {
-  const contact: Record<string, string> = {};
-  formData.forEach((value, key) => {
-    if (typeof value === 'string') {
-      contact[key] = value;
-    }
-  });
+  const contact = Object.fromEntries(formData);
   const result = contactSchema.safeParse(contact);
 
   if (!result.success) {
     return {
-      data: contact,
+      data: contact as ContactSchemaType,
       errors: result.error.formErrors,
       success: false,
     };
