d2e0f0de534f53e0e5305c1912e5d489558dc0a7
Aurora Scharff <aurora.sofie@gmail.com>
2024-08-02 19:45:41 +0200

    Update Button.tsx

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -5,12 +5,14 @@ export type Props = {
   type?: 'button' | 'submit' | 'reset';
   theme?: 'primary' | 'secondary' | 'destroy';
   children: React.ReactNode;
+  className?: string;
 };
 
 export default function Button({
   children,
   theme = 'primary',
   type,
+  className,
   ...otherProps
 }: Props & React.HTMLProps<HTMLButtonElement>) {
   const colorClass =
@@ -26,6 +28,7 @@ export default function Button({
       type={type}
       className={cn(
         colorClass,
+        className,
         'm-0 rounded-lg border-none px-3 py-2 font-medium shadow-sm hover:shadow-md active:shadow-xs active:enabled:translate-y-px disabled:translate-y-px disabled:shadow-xs',
       )}
     >
