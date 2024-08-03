27cc35c2c053af5d369bab278996f7b40ebfd49b
Aurora Scharff <aurora.sofie@gmail.com>
2024-08-03 17:22:09 +0200

    Update css

--- components/ContactButton.tsx
+++ components/ContactButton.tsx
@@ -16,7 +16,7 @@ export default function ContactButton({ contact }: { contact: Contact }) {
     <Link
       className={cn(
         isActive ? 'bg-primary text-white' : 'hover:bg-gray',
-        'flex w-full items-center justify-between gap-4 overflow-hidden whitespace-pre rounded-lg p-2 no-underline',
+        'flex w-full items-center justify-between gap-4 overflow-hidden whitespace-pre rounded-lg p-2 hover:no-underline',
       )}
       href={`/contacts/${contact.id}${query ? `?q=${query}` : ''}`}
     >
