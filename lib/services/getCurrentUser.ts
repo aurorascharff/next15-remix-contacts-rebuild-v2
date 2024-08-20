093af76e3800937efe3535bf0e81b19d42dbe66b
Aurora Scharff <aurora.sofie@gmail.com>
2024-08-20 20:26:35 +0200

    Dont get random user

--- lib/services/getCurrentUser.ts
+++ lib/services/getCurrentUser.ts
@@ -4,5 +4,5 @@ import { prisma } from '@/db';
 
 export async function getCurrentUser() {
   const users = await prisma.user.findMany();
-  return users[Math.floor(Math.random() * 2)];
+  return users[0];
 }
