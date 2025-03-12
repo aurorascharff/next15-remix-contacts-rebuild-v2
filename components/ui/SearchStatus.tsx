d9bd7edc83769419dfd9e484a6b7b29fdbcc0490
Aurora Scharff <aurora.sofie@gmail.com>
2025-03-12 21:08:34 +0100

    update

--- components/ui/SearchStatus.tsx
+++ components/ui/SearchStatus.tsx
@@ -1,7 +1,13 @@
+'use client';
+
 import React from 'react';
+import { useFormStatus } from 'react-dom';
 import { SearchIcon, SpinnerIcon } from './icons';
 
-export default function SearchStatus({ isSearching = false }: { isSearching?: boolean }) {
+export default function SearchStatus({ searching }: { searching: boolean }) {
+  const { pending } = useFormStatus();
+  const isSearching = searching || pending;
+
   return (
     <div aria-hidden="true" className="absolute left-10 top-7">
       {isSearching ? (
