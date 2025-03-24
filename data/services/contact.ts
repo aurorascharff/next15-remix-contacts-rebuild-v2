572af518224f93b0d5accf2a2a09dd85b23d063b
Aurora Scharff <aurora.sofie@gmail.com>
2025-03-24 19:12:48 +0100

    Update

--- data/services/contact.ts
+++ data/services/contact.ts
@@ -2,10 +2,10 @@ import 'server-only';
 
 import { prisma } from '@/db';
 
-export async function getContact(contactId: string) {
+export const getContact = (contactId: string) => {
   console.log('getContact', contactId);
   // TODO
-}
+};
 
 export async function getContacts() {
   return prisma.contact.findMany({
